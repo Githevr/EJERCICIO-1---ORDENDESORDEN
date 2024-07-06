e6c6826fcd56eff3089425802b22ef39b3ddb88e
Githevr <137414436+Githevr@users.noreply.github.com>
2024-07-05 20:04:52 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.btnMostrarDesordenado = new System.Windows.Forms.Button();
+            this.listBoxOutput = new System.Windows.Forms.ListBox();
+            this.btnMostrarOrdenado = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(115, 81);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(287, 111);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnIngresar.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.Location = new System.Drawing.Point(526, 48);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(180, 56);
+            this.btnIngresar.TabIndex = 4;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = false;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // btnMostrarDesordenado
+            // 
+            this.btnMostrarDesordenado.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnMostrarDesordenado.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrarDesordenado.Location = new System.Drawing.Point(526, 110);
+            this.btnMostrarDesordenado.Name = "btnMostrarDesordenado";
+            this.btnMostrarDesordenado.Size = new System.Drawing.Size(180, 56);
+            this.btnMostrarDesordenado.TabIndex = 5;
+            this.btnMostrarDesordenado.Text = "Mostrar Desordenado";
+            this.btnMostrarDesordenado.UseVisualStyleBackColor = false;
+            this.btnMostrarDesordenado.Click += new System.EventHandler(this.btnMostrarDesordenado_Click);
+            // 
+            // listBoxOutput
+            // 
+            this.listBoxOutput.BackColor = System.Drawing.SystemColors.Control;
+            this.listBoxOutput.FormattingEnabled = true;
+            this.listBoxOutput.Location = new System.Drawing.Point(316, 265);
+            this.listBoxOutput.Name = "listBoxOutput";
+            this.listBoxOutput.Size = new System.Drawing.Size(228, 173);
+            this.listBoxOutput.TabIndex = 6;
+            // 
+            // btnMostrarOrdenado
+            // 
+            this.btnMostrarOrdenado.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnMostrarOrdenado.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrarOrdenado.Location = new System.Drawing.Point(526, 172);
+            this.btnMostrarOrdenado.Name = "btnMostrarOrdenado";
+            this.btnMostrarOrdenado.Size = new System.Drawing.Size(180, 54);
+            this.btnMostrarOrdenado.TabIndex = 7;
+            this.btnMostrarOrdenado.Text = "Mostrar Ordenado";
+            this.btnMostrarOrdenado.UseVisualStyleBackColor = false;
+            this.btnMostrarOrdenado.Click += new System.EventHandler(this.btnMostrarOrdenado_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(225, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 23);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Datos";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(351, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 23);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Datos Ingresados";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMostrarOrdenado);
+            this.Controls.Add(this.listBoxOutput);
+            this.Controls.Add(this.btnMostrarDesordenado);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Button btnMostrarDesordenado;
+        private System.Windows.Forms.ListBox listBoxOutput;
+        private System.Windows.Forms.Button btnMostrarOrdenado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string[] array;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            array = txtInput.Text.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            MessageBox.Show("Array ingresado.");
+        }
+
+        private void btnMostrarDesordenado_Click(object sender, EventArgs e)
+        {
+            // Mostrar el array desordenado
+            listBoxOutput.Items.Clear();
+            foreach (var item in array)
+            {
+                listBoxOutput.Items.Add(item);
+            }
+        }
+
+        private void btnMostrarOrdenado_Click(object sender, EventArgs e)
+        {
+            // Ordenar el array usando recursividad y mostrarlo
+            var sortedArray = SortArray(array);
+            listBoxOutput.Items.Clear();
+            foreach (var item in sortedArray)
+            {
+                listBoxOutput.Items.Add(item);
+            }
+        }
+        private string[] SortArray(string[] arr)
+        {
+            // Implementar la recursión para ordenar el array
+            if (arr.Length <= 1)
+            {
+                return arr;
+            }
+
+            var pivot = arr[0];
+            var less = arr.Skip(1).Where(x => string.Compare(x, pivot) <= 0).ToArray();
+            var greater = arr.Skip(1).Where(x => string.Compare(x, pivot) > 0).ToArray();
+
+            return SortArray(less).Concat(new[] { pivot }).Concat(SortArray(greater)).ToArray();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
